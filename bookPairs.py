c9f157ada119d0021cebe3e966cecacd068c2ba4
jaivardhanf <jaivardhan.f@gmail.com>
2021-03-07 18:57:44 -0800

    Update bookPairs.py

--- bookPairs.py
+++ bookPairs.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext
 sc = SparkContext("local", "BookPairs")
 
 data = sc.textFile("goodreads.user.books1000")
-pairs = data.flatmap(lambda line: [(m,s) for m in line.split(":")[1].split(",") for s in line.split(":")[1].split(",") if s<m])
+pairs = data.flatMap(lambda line: [(m,s) for m in line.split(":")[1].split(",") for s in line.split(":")[1].split(",") if s<m])
 pairs1 = pairs.map(lambda p:(p,1))
 pairs0 = pairs1.reduceByKey(lambda a,b:a+b)
 pairs0.saveAsTextFile("output")
