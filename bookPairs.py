afc78ac221dd2cd3febeb21094a1570da47120f4
jaivardhanf <jaivardhan.f@gmail.com>
2021-03-07 19:19:19 -0800

    Update bookPairs.py

--- bookPairs.py
+++ bookPairs.py
@@ -5,6 +5,7 @@ data = sc.textFile("goodreads.user.books1000")
 pairs = data.flatMap(lambda line: [(m,s) for m in line.split(":")[1].split(",") for s in line.split(":")[1].split(",") if s<m])
 pairs1 = pairs.map(lambda p:(p,1))
 pairs0 = pairs1.reduceByKey(lambda a,b:a+b)
-pairs0.saveAsTextFile("output")
+pairs2 = pairs0.filter(lambda key,val -> Val>20)
+pairs2.saveAsTextFile("output")
 
 
